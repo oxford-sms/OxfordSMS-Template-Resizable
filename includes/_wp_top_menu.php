0353c155283430f9fbd3ff090f2c9121883b87ef
JoomlaBoat <ivankomlev@gmail.com>
2020-09-04 11:08:33 -0500

    Santiago links changed

--- includes/_wp_top_menu.php
+++ includes/_wp_top_menu.php
@@ -68,34 +68,20 @@
                                     <li  id="menu-item-2393"  class="unused-10234789 menu-item-type-post_type menu-item-object-page menu-item-2393 fusion-dropdown-submenu" ><a  href="https://david.oxford.edu.pa/application-form" class="fusion-bottombar-highlight"><span>David</span></a></li>
                                     <li  id="menu-item-2193"  class="unused-10234789 menu-item-type-post_type menu-item-object-page menu-item-2193 fusion-dropdown-submenu" ><a  href="https://www.oxford.edu.pa/panama-application-form" class="fusion-bottombar-highlight"><span>Panama</span></a></li>
 									<li  id="menu-item-7033"  class="unused-10234789 menu-item-type-post_type menu-item-object-page menu-item-7033 fusion-dropdown-submenu" ><a  href="https://pdn.oxford.edu.pa/application-form" class="fusion-bottombar-highlight"><span>Paseo del Norte</span></a></li>
-                                    <li  id="menu-item-2383"  class="unused-10234789 menu-item-type-post_type menu-item-object-page menu-item-2383 fusion-dropdown-submenu" ><a  href="https://www.oxford.edu.pa/santiago-application-form" class="fusion-bottombar-highlight"><span>Santiago</span></a></li>
+                                    <li  id="menu-item-2383"  class="unused-10234789 menu-item-type-post_type menu-item-object-page menu-item-2383 fusion-dropdown-submenu" ><a  href="https://santiago.oxford.edu.pa/application-form" class="fusion-bottombar-highlight"><span>Santiago</span></a></li>
                                  </ul>
                               </li>
 							  
-							  <!--<li  id="menu-item-317"  class="parents unused-10234789 menu-item-type-custom menu-item-object-custom current-menu-item current-menu-ancestor current-menu-parent menu-item-has-children menu-item-317 fusion-dropdown-menu fusion-menu-item-button"  data-classes="parents" data-item-id="317">-->
 							  <li  id="menu-item-123"  class="unused-10234789 menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-123 fusion-dropdown-menu"  data-item-id="123">
                                  <a href="https://oxford.edu.pa/panama/" class="fusion-bottombar-highlight">
 								 <span class="menu-text">Parent Portal</span> <span class="fusion-caret"><i class="fusion-dropdown-indicator"></i></span></a>
-								 
-								 <!--<span class="menu-text fusion-button button-portal button-small"><!--<span class="button-icon-divider-left"><i class="glyphicon fa-angle-down fas"></i></span>-->
-								 
-								 <!--<span class="fusion-button-text-left">-->
-								 
-								 <!--
-						 
-								 <span class="fusion-caret"><i class="fusion-dropdown-indicator"></i></span>
-								 </span>
-								 -->
-								 
-								 
-								 
-								 </a>
+
                                  <ul class="sub-menu">
                                     <li  id="menu-item-319"  class="unused-10234789 menu-item-type-custom menu-item-object-custom menu-item-319 fusion-dropdown-submenu" ><a  href="https://azuero.oxford.edu.pa/" class="fusion-bottombar-highlight"><span>Azuero</span></a></li>
                                     <li  id="menu-item-320"  class="unused-10234789 menu-item-type-custom menu-item-object-custom menu-item-320 fusion-dropdown-submenu" ><a  href="https://david.oxford.edu.pa/" class="fusion-bottombar-highlight"><span>David</span></a></li>
                                     <li  id="menu-item-318"  class="unused-10234789 menu-item-type-custom menu-item-object-custom current-menu-item menu-item-318 fusion-dropdown-submenu" ><a  href="https://www.oxford.edu.pa/panama" class="fusion-bottombar-highlight"><span>Panama City</span></a></li>
                                     <li  id="menu-item-1693"  class="unused-10234789 menu-item-type-post_type menu-item-object-page menu-item-1693 fusion-dropdown-submenu" ><a  href="https://pdn.oxford.edu.pa/" class="fusion-bottombar-highlight"><span>Paseo del Norte</span></a></li>
-                                    <li  id="menu-item-324"  class="unused-10234789 menu-item-type-custom menu-item-object-custom menu-item-324 fusion-dropdown-submenu" ><a  href="https://www.oxford.edu.pa/santiago" class="fusion-bottombar-highlight"><span>Santiago</span></a></li>
+                                    <li  id="menu-item-324"  class="unused-10234789 menu-item-type-custom menu-item-object-custom menu-item-324 fusion-dropdown-submenu" ><a  href="https://santiago.oxford.edu.pa/" class="fusion-bottombar-highlight"><span>Santiago</span></a></li>
                                     <li  id="menu-item-5383"  class="unused-10234789 menu-item-type-post_type menu-item-object-page menu-item-5383 fusion-dropdown-submenu" ><a  href="https://www.oxford.edu.pa/the-moodle-platform/" class="fusion-bottombar-highlight"><span>The Moodle Platform</span></a></li>
                                  </ul>
                               </li>
